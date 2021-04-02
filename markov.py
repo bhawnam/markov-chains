d795a2138d8d75997106dc98cade98f749450329
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-04-02 20:37:37 +0000

    Added another input_path file to test the output

--- markov.py
+++ markov.py
@@ -81,6 +81,7 @@ def make_text(chains):
 
 
 input_path = 'green-eggs.txt'
+# input_path = 'gettysburg.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
